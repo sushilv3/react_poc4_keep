8273e8bdef7614940f023a273cde09f5501b5254
sushilv3 <sushilv3rma@gmail.com>
2021-05-17 09:02:39 +0530

    added all custom components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CreateNote from './components/CreateNote';
+import Note from './components/Note';
 import './App.css';
 
 function App() {
-    return ( <h1> App component </h1>
+    const[addItem, setAddItem] = useState([]);
+
+    const addNote = (note)=>{
+        // alert('i m clickc');
+     
+        setAddItem((prevData)=>{
+            return[...prevData, note];
+        });
+        console.log(note);
+
+    };
+
+    const onDelete = (id) =>{
+        setAddItem((oldData)=>oldData.filter((currValaue, index)=>{
+            return index !==id;
+        }));
+
+    };
+    return ( 
+        <>
+            <h1>  </h1>
+            <Header />
+            <CreateNote passNote={addNote} />
+            {addItem.map((currValue,index)=>{return (<Note key={index} id={index} title={currValue.title} content={currValue.content} deleteItem={onDelete}/>);})}
+          
+            <Footer />
+        </>
     );
 }
 
